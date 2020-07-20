30a74f59302dc68ebb0c0ec8e3e5ee4465954406
winebarrel <sugawara@winebarrel.jp>
2020-07-20 11:07:26 +0900

    Support SPRING_ONION_DATABASE_URL

--- lib/spring_onion.rb
+++ lib/spring_onion.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 require 'logger'
-require 'coderay'
+
 require 'active_support'
+require 'coderay'
+require 'mysql2'
 
 require 'spring_onion/config'
 require 'spring_onion/error'
@@ -11,6 +13,12 @@ require 'spring_onion/json_logger'
 require 'spring_onion/version'
 
 ActiveSupport.on_load :active_record do
+  if ENV['SPRING_ONION_DATABASE_URL'] && !SpringOnion.connection
+    SpringOnion.connection = Mysql2::Client.new(
+      ActiveRecord::ConnectionAdapters::ConnectionSpecification::ConnectionUrlResolver.new(url).to_hash
+    )
+  end
+
   require 'active_record/connection_adapters/abstract_mysql_adapter'
   ActiveRecord::ConnectionAdapters::AbstractMysqlAdapter.prepend SpringOnion::Explainer
 end
